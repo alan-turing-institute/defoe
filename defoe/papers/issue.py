54f07b9383fedc289c4d5c9a1b4886b658491b51
Mike Jackson <michaelj@epcc.ed.ac.uk>
2019-01-24 16:38:22 +0000

    Improved defoe.papers.issue comments

--- defoe/papers/issue.py
+++ defoe/papers/issue.py
@@ -1,9 +1,14 @@
 """
-Object module representation of XML issue of newspaper.
+Object model representation of an issue of a newspaper represented as
+an XML document.
+
+The XML document can conform to the following schemas:
+
+* British Library Newspapers
+* Times Digital Archive
 """
 
 from datetime import datetime
-from logging import getLogger
 from lxml import etree
 
 from defoe.papers.article import Article
@@ -12,28 +17,34 @@ from defoe.spark_utils import open_stream
 
 class Issue(object):
     """
-    Object module representation of XML issue of newspaper.
+    Object model representation of an issue of a newspaper represented
+    as an XML document.
     """
 
     def __init__(self, filename):
-        self.logger = getLogger('py4j')
+        """
+        Constructor. If the filename cannot be parsed into valid XML
+        an empty document is created.
+
+        :param filename: XML filename
+        :type: filename: str or unicode
+        """
         self.filename = filename
         stream = open_stream(self.filename)
-        # Try hard to parse the file, even if it looks like this:
-        # <wd pos="1664,5777,2052,5799">Bart,OwnerndPetitioner.Take/wd>
+
+        self.issue_tree = None
+        self.issue = ''
+        self.newspaper_id = ''
+        self.articles = []
+        self.date = None
+        self.page_count = 0
+        self.day_of_week = ''
+        # Attempt to parse the file, even if its XML is invalid e.g:
+        # <wd ...>.../wd>
         parser = etree.XMLParser(recover=True)
         try:
-            self.tree = etree.parse(stream, parser)
-        except etree.XMLSyntaxError as error:
-            self.logger.error('Error when parsing: %s',
-                              error.msg)
-            self.tree = None
-            self.issue = ''
-            self.newspaper_id = ''
-            self.articles = []
-            self.date = datetime.now()
-            self.page_count = 0
-            self.day_of_week = ''
+            self.issue_tree = etree.parse(stream, parser)
+        except etree.XMLSyntaxError:
             return
 
         try:
@@ -43,39 +54,55 @@ class Issue(object):
             # BLN: /issue/article/text/*/p/wd
             self.issue = self.single_query('/issue')
 
-        self.newspaper_id = ''
         # GALENP: /GALEN/Newspaper/issue/metadatainfo/newspaperID
         newspaper_id = self.single_query('//newspaperID/text()')
         if newspaper_id is None:
             # BLN: /issue/newspaperId
             newspaper_id = self.single_query('//newspaperId/text()')
-        if not newspaper_id is None:
+        if newspaper_id is not None:
             self.newspaper_id = newspaper_id
 
         self.articles = [Article(article, self.filename)
                          for article in self.query('.//article')]
+
         raw_date = self.single_query('//pf/text()')
         if raw_date:
             self.date = datetime.strptime(raw_date, '%Y%m%d')
-        else:
-            self.date = None
+
         try:
             self.page_count = int(self.single_query('//ip/text()'))
-        except Exception as error:
-            self.logger.error('Failed to get page count: %s', error)
-            self.page_count = 0
+        except Exception:
+            pass
 
         self.day_of_week = self.single_query('//dw/text()')
 
     def query(self, query):
-        if not self.tree:
+        """
+        Run XPath query.
+
+        :param query: XPath query
+        :type query: str or unicode
+        :return: list of query results or an empty list if the object
+        represents an empty document or any errors arose
+        :rtype: list(lxml.etree.<MODULE>) (depends on query)
+        """
+        if not self.issue_tree:
             return []
         try:
-            return self.tree.xpath(query)
+            return self.issue_tree.xpath(query)
         except AssertionError:
             return []
 
     def single_query(self, query):
+        """
+        Run XPath query and return first result.
+
+        :param query: XPath query
+        :type query: str or unicode
+        :return: query results or None if the object represents an
+        empty document or any errors arose
+        :rtype: str or unicode
+        """
         result = self.query(query)
         if not result:
             return None
@@ -85,9 +112,22 @@ class Issue(object):
             return unicode(result[0])
 
     def __getitem__(self, index):
+        """
+        Given an article index, return the requested article.
+
+        :param index: article index
+        :type index: int
+        :return: Article object
+        :rtype: defoe.alto.article.Article
+        """
         return self.articles(index)
 
     def __iter__(self):
-        # Iterate through all the articles
+        """
+        Iterate over articles.
+
+        :return: Article object
+        :rtype: defoe.alto.article.Article
+        """
         for article in self.articles:
             yield article
