06a6c44d84756175449042ac4fa3e18828eb558d
Mike Jackson <michaelj@epcc.ed.ac.uk>
2019-01-24 16:46:32 +0000

    Improved commenting of defoe.papers.article

--- defoe/papers/article.py
+++ defoe/papers/article.py
@@ -1,62 +1,83 @@
 """
-Object model representation of XML Article.
-"""
+Object model representation of an article of a newspaper represented
+as an XML document.
 
-from logging import getLogger
+The XML document can conform to the following schemas:
+
+* British Library Newspapers
+* Times Digital Archive
+"""
 
 
 class Article(object):
     """
-    Class to represent an article in an issue of an newspaper
+    Object model representation of an article of a newspaper
+    represented as an XML document.
     """
-    def __init__(self, source, filename):
+
+    def __init__(self, article_tree, filename):
         """
-        Create the article from source XML
+        Constructor.
+
+        :param article_tree: article XML
+        :type article_tree: lxml.etree._Element
+        :param filename: file from which the article XML was extracted
+        :type: filename: str or unicode
         """
-        self.logger = getLogger('py4j')
-        self.tree = source
+        self.article_tree = article_tree
         self.filename = filename
-        self.quality = self.tree.xpath('ocr/text()')
+        self.quality = self.article_tree.xpath('ocr/text()')
         if not self.quality:
             self.quality = None
         elif len(self.quality) == 1:
             self.quality = float(self.quality[0])
         else:
-            self.logger.info('Multiple OCR qualities found. Dropping.')
             self.quality = None
-        self.title = self.tree.xpath('text/text.title/p/wd/text()')
-        self.preamble = self.tree.xpath('text/text.preamble/p/wd/text()')
-        self.content = self.tree.xpath('text/text.cr/p/wd/text()')
+        self.title = self.article_tree.xpath('text/text.title/p/wd/text()')
+        self.preamble = self.article_tree.xpath(
+            'text/text.preamble/p/wd/text()')
+        self.content = self.article_tree.xpath('text/text.cr/p/wd/text()')
         self.article_id = ""
-        article_id = self.tree.xpath('id/text()')
+        article_id = self.article_tree.xpath('id/text()')
         if article_id:
             self.article_id = article_id[0]
         self.page_ids = []
-        pi = self.tree.xpath('pi/text()')
+        pi_text = self.article_tree.xpath('pi/text()')
         splitter = None
-        if pi:
-            if "_" in pi[0]:
+        if pi_text:
+            if "_" in pi_text[0]:
                 # BLN
                 splitter = "_"
-            elif "-" in pi[0]:
+            elif "-" in pi_text[0]:
                 # GALENP
                 splitter = "-"
             if splitter:
-                for p in pi:
-                    self.page_ids.append(p.split(splitter)[-1])
+                for page_id in pi_text:
+                    self.page_ids.append(page_id.split(splitter)[-1])
 
     @property
     def words(self):
         """
-        Get the full text of the article, title etc.abs as a list of tokens
+        Get the full text of the article - the title, preamble and
+        content - as a list of strings.
+
+        :return: full text
+        :rtype: list(str or unicode)
         """
         return self.title + self.preamble + self.content
 
     @property
     def words_string(self):
         """
-        Return the full text of the article as a string. Remove all hyphens.
-        This merges hyphenated word by may cause problems with subordinate
-        clauses (The sheep - the really loud one - had just entered my office).
+        Get the full text of the article - the title, preamble and
+        content - as a single string, concatenated by spaces and with
+        hyphenation removed.
+
+        Note: merging hyphenated words may cause problems with
+        subordinate clauses e.g. "The sheep - the really aloud one -
+        had just entered my office".
+
+        :return: full text
+        :rtype: str or unicode
         """
         return ' '.join(self.words).replace(' - ', '')
