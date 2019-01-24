10fe5856aa0e9c3c731f1b55a5693daa71ae6f62
Mike Jackson <michaelj@epcc.ed.ac.uk>
2019-01-24 15:42:38 +0000

    Improved commenting and extended tests in defoe/test/alto/test_page.py

--- defoe/test/alto/test_page.py
+++ defoe/test/alto/test_page.py
@@ -15,8 +15,42 @@ class TestPage(TestCase):
     """
 
     def setUp(self):
+        """
+        Creates page from test file fixtures/000000037_000005.xml.
+        """
         source = get_path(fixtures, '000000037_000005.xml')
         self.page = Page(None, None, source)
 
+    def test_words_count(self):
+        """
+        Tests Page.words property returns expected number of words.
+        """
+        self.assertEqual(52, len(self.page.words))
+
+    def test_words_words(self):
+        """
+        Tests Page.words property includes expected words.
+        """
+        words = self.page.words
+        self.assertTrue("BEFORE" in words)
+        self.assertTrue("YOU" in words)
+        self.assertTrue("BUY" in words)
+
+    def test_strings_count(self):
+        """
+        Tests Page.strings property returns expected number of strings.
+        """
+        self.assertEqual(52, len(self.page.strings))
+
+    def test_images_count(self):
+        """
+        Tests Page.strings property returns expected number of images.
+        """
+        self.assertEqual(0, len(self.page.images))
+
     def test_content(self):
+        """
+        Tests Page.content property returns text which includes an
+        expected phrase.
+        """
         self.assertTrue("BEFORE YOU BUY" in self.page.content)
