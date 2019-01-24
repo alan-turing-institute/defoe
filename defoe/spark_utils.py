f2ccaa962edd231edc4d3ef14b0ede8ded8db17f
Mike Jackson <michaelj@epcc.ed.ac.uk>
2019-01-24 10:21:13 +0000

    Improved commenting of spark_utils.py

--- defoe/spark_utils.py
+++ defoe/spark_utils.py
@@ -1,5 +1,5 @@
 """
-File-handling utilities.
+Spark-related file-handling utilities.
 """
 
 HTTP = "http://"
@@ -11,7 +11,19 @@ def files_to_rdd(context,
                  num_cores=1,
                  data_file="data.txt"):
     """
-    Populate Spark RDD with file names over which query is to be run.
+    Populate Spark RDD with file names or URLs over which a query is
+    to be run.
+
+    :param context: Spark Context
+    :type context: pyspark.context.SparkContext
+    :param num_cores: number of cores over which to parallelize Spark
+    job
+    :type num_cores: int
+    :param data_file: name of file with file names or URLs, one per
+    line
+    :type data_file: str or unicode
+    :return: RDD
+    :rtype: pyspark.rdd.RDD
     """
     filenames = [filename.strip() for filename in list(open(data_file))]
     rdd_filenames = context.parallelize(filenames, num_cores)
@@ -20,7 +32,26 @@ def files_to_rdd(context,
 
 def open_stream(filename):
     """
-    Open a file and return a stream.
+    Open a file and return a stream to the file.
+
+    If filename starts with "http:" or "https:" then file is assumed
+    to be a URL.
+
+    If filename starts with "blob:" then file is assumed to be held
+    within Azure as a BLOB. This expects the following environment
+    variables to be set:
+
+    * BLOB_SAS_TOKEN
+    * BLOB_ACCOUNT_NAME
+    * BLOB_CONTAINER_NAME
+
+    Otherwise, the filename is assumed to be held on the file
+    system.
+
+    :param filename: file name or URL
+    :type filename: str or unicode
+    :return: open stream
+    :rtype: cStringIO.StringI (URL or file system) OR io.BytesIO (blob)
     """
     assert filename, "Filename must not be ''"
 
@@ -45,11 +76,13 @@ def open_stream(filename):
         if sas_token[0] == '?':
             sas_token = sas_token[1:]
 
-        blob_service = BlobService(account_name=os.environ['BLOB_ACCOUNT_NAME'],
-                                   sas_token=sas_token)
+        blob_service = BlobService(
+            account_name=os.environ['BLOB_ACCOUNT_NAME'],
+            sas_token=sas_token)
         filename = filename[len(BLOB):]
-        blob = blob_service.get_blob_to_bytes(os.environ['BLOB_CONTAINER_NAME'],
-                                              filename)
+        blob = blob_service.get_blob_to_bytes(
+            os.environ['BLOB_CONTAINER_NAME'],
+            filename)
         stream = io.BytesIO(blob)
 
     else:
