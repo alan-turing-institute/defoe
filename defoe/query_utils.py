dc023a29054b45fc680ac5ed7c2de2d9dfd704b1
DR Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2019-02-08 11:57:26 +0000

    preprocessing utils

--- defoe/query_utils.py
+++ defoe/query_utils.py
@@ -3,6 +3,8 @@ Query-related utility functions.
 """
 
 import re
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from nltk.tokenize import word_tokenize
 
 NON_AZ_REGEXP = re.compile('[^a-z]')
 
@@ -18,3 +20,61 @@ def normalize(word):
     :rtype word: str or unicode
     """
     return re.sub(NON_AZ_REGEXP, '', word.lower())
+
+def stemming(word):
+    """
+    Stemming is a process of reducing words to their word stem, 
+    base or root form (for example, books — book, looked — look). 
+    The main two algorithms are Porter stemming algorithm 
+    (removes common morphological and inflexional endings from words - Used here),
+     and Lancaster stemming algorithm (a more aggressive stemming algorithm). 
+
+    :param word: Word to stemm
+    :type word: str or unicode
+    :return: normalized word
+    :rtype word: str or unicode
+    """
+
+    stemmer= PorterStemmer()
+    return stemmer.stem(word)
+
+def lemmatization(word):
+    """
+    The aim of lemmatization, like stemming, is to reduce inflectional forms to a common base form.
+    As opposed to stemming, lemmatization does not simply chop off inflections. 
+    Instead it uses lexical knowledge bases to get the correct base forms of words.
+
+
+    :param word: Word to normalize
+    :type word: str or unicode
+    :return: normalized word
+    :rtype word: str or unicode
+    """
+    lemmatizer=WordNetLemmatizer()
+    return lemmatizer.lemmatize(word)
+
+def preprocess_word(word, prep_type):
+    """
+    Preprocess a word by applying different treatments depending on the prep_type paramenter.
+    (0) normalize; (1) normalize + stemming; (2) normalize + lemmatization; (other value) word. 
+
+    :param word: word
+    :type word: string
+    :param prep_type: 0 to apply just normalize. 1 to apply normalize and stemming, 2 to apply normalize and lemmatization. Otherwise, it just returns the original word.
+    :type integer 
+    :return: words
+    :rtype: string
+    """
+
+    normalized_word = normalize(word)
+    if prep_type == 0:
+        preprocessed_word = normalized_word
+    elif prep_type == 1:
+       preprocessed_word = stemming(normalized_word)
+    elif prep_type == 2:
+       preprocessed_word = lemmatization(normalized_word)
+    else:
+       preprocessed_word = word
+    return preprocessed_word
+
+
