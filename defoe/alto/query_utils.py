a15bb429ee2acacb14b8b36a7315702069d058f8
DR Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2019-02-17 15:30:01 +0000

    preprocessing books

--- defoe/alto/query_utils.py
+++ defoe/alto/query_utils.py
@@ -3,6 +3,12 @@ Query-related utility functions.
 """
 
 from defoe import query_utils
+"""
+prep_type: integer variable, which indicates the type of preprocess treatment
+to appy to each word. normalize(0); normalize + stemming (1); normalize + lemmatization (2); (other value) original word. 
+
+"""
+prep_type= 1
 
 
 def get_page_matches(document, keywords):
@@ -30,10 +36,52 @@ def get_page_matches(document, keywords):
         for page in document:
             match = None
             for word in page.words:
-                if query_utils.normalize(word) == keyword:
+                preprocessed_word = query_utils.preprocess_word(word, prep_type)
+                if preprocessed_word == keyword:
                     match = (document.year, document, page, keyword)
                     break
             if match:
                 matches.append(match)
                 continue  # move to next page
     return matches
+
+def get_document_keywords(document, keywords):
+    """
+    Gets list of keywords occuring within an document.
+
+    Article words are normalized, by removing all non-'a-z|A-Z'
+    characters.
+
+    :param article: Article
+    :type article: defoe.papers.article.Article
+    :param keywords: keywords
+    :type keywords: list(str or unicode)
+    :return: sorted list of keywords that occur within article
+    :rtype: list(str or unicode)
+    """
+    matches = set()
+    for page in document:
+       for word in page.words:
+            preprocessed_word = query_utils.preprocess_word(word, prep_type)
+            if preprocessed_word in keywords:
+                matches.add(preprocessed_word)
+    return sorted(list(matches))
+
+
+def document_contains_word(document, keyword):
+    """
+    Checks if a keyword occurs within an article.
+
+    :param article: Article
+    :type article: defoe.papers.article.Article
+    :param keywords: keyword
+    :type keywords: str or unicode
+    :return: True if the article contains the word, false otherwise
+    :rtype: bool
+    """
+    for page in document:
+        for word in page.words:
+            preprocessed_word = query_utils.preprocess_word(word, prep_type)
+            if keyword == preprocessed_word:
+                return True
+    return False
