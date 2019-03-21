b3b2d3e2c14c494ae7730e770f5dcad6c89753d2
rosafilgueira <rosa.filgueira@ed.ac.uk>
2019-03-21 12:22:27 +0000

    supporting filename and ocr in target_concordance_collocation

--- defoe/papers/queries/target_concordance_collocation_by_date.py
+++ defoe/papers/queries/target_concordance_collocation_by_date.py
@@ -5,7 +5,6 @@ which have a target word and groups the results by date.
 Words in articles, target words and keywords can be normalized,
 normalized and stemmed, or normalized and lemmatized (default).
 """
-
 import os.path
 import yaml
 
@@ -19,42 +18,33 @@ def do_query(issues, config_file=None, logger=None):
     """
     Gets concordance and collocation for keywords occurring in
     articles which have a target word and groups the results by date.
-
     Words in articles, target words and keywords can be normalized,
     normalized and stemmed, or normalized and lemmatized (default).
-
     A window size (default 10) determines the size of the concordance
     returned.
-
     config_file must be the path to a configuration file of form:
-
         preprocess: none|normalize|stem|lemmatize # Optional
         window: <WINDOW_SIZE> # Optional
         data: <DATA_FILE>
-
     <DATA_FILE> must be the path to a plain-text data file with a list
     of keywords to search for, one per line. The first word is assumed
     to be the target word. If <DATA_FILE> is a relative path then it
     is assumed to be relative to the directory in which config_file
     resides.
-
     config_file must be the path to a configuration file with a list
     of the keywords to search for, one per line. The first word
     is assumed to be the target word.
-
     Returns result of form:
-
         {
             <YEAR>:
             [
-                [<WORD>, <CONCORDANCE>],
-                [<WORD>, <CONCORDANCE>],
+                [<FILENAME>,<WORD>, <CONCORDANCE>,<OCR>],
+                [<FILENAME>,<WORD>, <CONCORDANCE>,<OCR>],
                 ...
             ],
             <YEAR>:
             ...
         }
-
     :param issues: RDD of defoe.alto.issue.Issue
     :type issues: pyspark.rdd.PipelinedRDD
     :param config_file: query configuration file
@@ -72,38 +62,42 @@ def do_query(issues, config_file=None, logger=None):
                                               os.path.dirname(config_file))
     window = query_utils.extract_window_size(config)
     keywords = []
-    with open(data_file, 'r') as f:
+    with open(data_file, "r") as f:
         keywords = [query_utils.preprocess_word(
             word, preprocess_type) for word in list(f)]
     target_word = keywords[0]
-
-    # [(year, article), ...]
+    # [(year, article, filename, ocr), ...]
     articles = issues.flatMap(
-        lambda issue: [(issue.date.year, article)
+        lambda issue: [(issue.date.year, article, issue.filename, article.quality)
                        for article in issue.articles])
-
-    # [(year, article), ...]
+    # [(year, article,filename, ocr), ...]
     target_articles = articles.filter(
         lambda year_article: article_contains_word(
             year_article[1], target_word, preprocess_type))
+    
 
-    # [(year, (article, [(word, idx), (word, idx) ...]), ...]
+    # [(year, (article, filename, [(word, idx), (word, idx) ...], ocr), ...]
     matching_idx = target_articles.map(
         lambda year_article: (
             (year_article[0],
-             get_article_idx(year_article[1], keywords, preprocess_type))))
+             get_article_idx(year_article[1],year_article[2], keywords, year_article[3], preprocess_type))
+            ))
+    
 
-    # [(year, (word, corcondance), (word, concordance) ...), ...]
+    # [(year, (filename, word, corcondance, ocr), (filename, word, concordance, ocr) ...), ...]
     concordance_words = matching_idx.flatMap(
         lambda target_article: [
-            (target_article[0],
+            (target_article[0], 
              get_concordance(target_article[1][0],
-                             match,
+                             target_article[1][1],
+                             match, 
                              window,
+                             target_article[1][3],
                              preprocess_type))
-            for match in target_article[1][1]])
-
-    # [(year, [word, concodance], [word, concordance]), ...]
+            for match in target_article[1][2]])
+    
+    
+    # [(year,(filename, [word, concodance], ocr), (filename,[word, concordance], ocr), ...]
     result = concordance_words.groupByKey() \
         .map(lambda year_wordcount:
              (year_wordcount[0], list(year_wordcount[1]))) \
