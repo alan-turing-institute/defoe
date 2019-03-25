d6667cb61e80195c5680359f32b6afd01ce63323
Mike Jackson <michaelj@epcc.ed.ac.uk>
2019-03-25 13:44:09 +0000

    Modified code, docs and tests to add support for LTO_issue.dtd schema used in another TDA dataset

--- defoe/test/papers/test_article_lto.py
+++ defoe/test/papers/test_article_lto.py
@@ -1,6 +1,6 @@
 """
 defoe.papers.article.article tests for newspapers conforming to
-bl_ncnp_issue_apex.dtd.
+LTO_issue.dtd.
 """
 
 from defoe.papers.issue import Issue
@@ -9,10 +9,10 @@ from defoe.test.papers import fixtures
 from defoe.test.papers.test_article import TestArticle
 
 
-class TestArticleBl(TestArticle):
+class TestArticleLto(TestArticle):
     """
     defoe.papers.article.article tests for newspapers conforming to
-    bl_ncnp_issue_apex.dtd.
+    LTO_issue.dtd.
     """
 
     __test__ = True
@@ -22,8 +22,8 @@ class TestArticleBl(TestArticle):
         Creates Issue from test file fixtures/1912_11_10_bl.xml then
         retrieves first Article.
         """
-        self.filename = get_path(fixtures, '1912_11_10_bl.xml')
+        self.filename = get_path(fixtures, '1912_11_10_lto.xml')
         issue = Issue(self.filename)
         self.article = issue.articles[0]
-        self.article_id = 'NID123_19121110_0001-001'
-        self.page_id = '0001'
+        self.article_id = 'NID123-1912-NOV10-001-001'
+        self.page_id = '001'
