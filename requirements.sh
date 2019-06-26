887262ea247926758d896b16ec17c355ba3cbcd2
rosafilgueira <rosa.filgueira@epcc.ed.ac.uk>
2019-06-26 17:36:09 +0100

    requirments in a bash script

---
+++ requirements.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+/usr/bin/anaconda/bin/conda install --yes lxml
+/usr/bin/anaconda/bin/conda install --yes nltk
+/usr/bin/anaconda/bin/conda install --yes pep8
+/usr/bin/anaconda/bin/conda install --yes pylint
+/usr/bin/anaconda/bin/conda install --yes pycodestyle
+/usr/bin/anaconda/bin/conda install --yes pytest
+/usr/bin/anaconda/bin/conda install --yes PyYAML
+/usr/bin/anaconda/bin/conda install --yes regex
+/usr/bin/anaconda/bin/conda install --yes requests
