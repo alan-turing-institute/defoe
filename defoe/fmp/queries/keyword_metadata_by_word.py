cf68631b789101f8b62cbf968a36812f51e34329
rosafilgueira <rosa.filgueira@ed.ac.uk>
2019-08-08 14:27:35 +0100

    improving doc

--- defoe/fmp/queries/keyword_metadata_by_word.py
+++ defoe/fmp/queries/keyword_metadata_by_word.py
@@ -41,7 +41,7 @@ def do_query(archives, config_file=None, logger=None):
           ...
         }
 
-    :param archives: RDD of defoe.alto.archive.Archive
+    :param archives: RDD of defoe.fmp.archive.Archive
     :type archives: pyspark.rdd.PipelinedRDD
     :param config_file: query configuration file
     :type config_file: str or unicode
