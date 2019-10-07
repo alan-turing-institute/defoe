f89f03c7b38f345c8e4d13ec57a98ceb5786d09a
rosafilgueira <rosa.filgueira@ed.ac.uk>
2019-10-07 20:00:15 +0100

    improving finding exact matches

--- defoe/nls/query_utils.py
+++ defoe/nls/query_utils.py
@@ -186,9 +186,15 @@ def get_sentences_list_matches(text, keysentence):
     match = []
     text_list= text.split()
     for sentence in keysentence:
-        pattern = re.compile(r'^%s$'%sentence)
-        for word in text_list:
-            if re.search(pattern, word):
-                match.append(sentence)
+        if len(sentence.split()) > 1:
+            if sentence in text:
+                count = text.count(sentence)
+                for i in range(0, count):
+                    match.append(sentence)
+        else:
+            pattern = re.compile(r'^%s$'%sentence)
+            for word in text_list:
+                if re.search(pattern, word):
+                    match.append(sentence)
     return sorted(match)
 
