94ac3effdb48f85524602e26dff92aa601e788fd
rosafilgueira <rosa.filgueira@ed.ac.uk>
2019-10-10 18:15:42 +0100

    improving the query

--- defoe/nls/queries/write_pages.py
+++ defoe/nls/queries/write_pages.py
@@ -12,8 +12,14 @@ def do_query(archives, config_file=None, logger=None):
     Writes raw pages as string to HDFS textfiles, and some metadata associated with each document.
 
     Non preprocessed steps are applied to the words extracted from pages.
-    Metadata collected: Tittle, year, place, archive filename, page filename, type of archive
- 
+    Metadata collected: Tittle, year, place, archive filename, page filename, page id, num pages, type of archive
+    
+    Example:
+    ('Encyclopaedia Britannica; or, A dictionary of arts, sciences, and miscellaneous literature', 
+      1810, 'Edinburgh', '/mnt/lustre/at003/at003/rfilguei2/nls-data-encyclopaediaBritannica/191253839', 
+      'alto/192209952.34.xml', 'Page5', 446, 'book', u"Part III. MORAL PHILOSOPHY.....)
+    ( .... 'alto/192209965.34.xml', 'Page6', 446, 'book', u'402 1 24\xb0 Belief of immortali- ty, &c. a great fu ,>- port- amidfl trials.)
+    
     :param archives: RDD of defoe.nls.archive.Archive
     :type archives: pyspark.rdd.PipelinedRDD
     :param config_file: query configuration file
@@ -29,9 +35,9 @@ def do_query(archives, config_file=None, logger=None):
         lambda archive: [(document.title, document.year, document.place, document.archive.filename, document.num_pages, document.document_type,  document) for document in list(archive)])
     # [(year, title, place, publisher, date, page_string)
     pages = documents.flatMap(
-        lambda year_document: [(year_document[0],year_document[1], year_document[2], year_document[3], page.code, year_document[4], year_document[5],   
+        lambda year_document: [(year_document[0],year_document[1], year_document[2], year_document[3], page.code, page.page_id, year_document[4], year_document[5],   
                                     get_page_as_string(page, preprocess_type)) 
                                        for page in year_document[6]])
 
-    pages.saveAsTextFile("hdfs:///user/at003/rosa/text12.txt")
+    pages.saveAsTextFile("hdfs:///user/at003/rosa/text15.txt")
     return "0"
