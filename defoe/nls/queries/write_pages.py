7dbee6f1bbbe71257c4ed099907c989b56e826f1
rosafilgueira <rosa.filgueira@ed.ac.uk>
2019-10-10 17:34:56 +0100

    better metadata capture

--- defoe/nls/queries/write_pages.py
+++ defoe/nls/queries/write_pages.py
@@ -12,9 +12,7 @@ def do_query(archives, config_file=None, logger=None):
     Writes raw pages as string to HDFS textfiles, and some metadata associated with each document.
 
     Non preprocessed steps are applied to the words extracted from pages.
-    Metadata associated: Year (e.g. 1810), title (e.g. Encyclopaedia Britannica; or, A dictionary of arts, sciences, and miscellaneous literature),
-                         place (e.g. stk), publisher (none), date of the document (1810). 
-    And page as string:  'Part III. MORAL PHILOSOPHY. 22,6 Unfat isfied defires of exiftence and happi- \xbbeis. Motives to ^ood minds, and feme traces of which arc found in the Virtue.'
+    Metadata collected: Tittle, year, place, archive filename, page filename, type of archive
  
     :param archives: RDD of defoe.nls.archive.Archive
     :type archives: pyspark.rdd.PipelinedRDD
@@ -28,12 +26,12 @@ def do_query(archives, config_file=None, logger=None):
     # [(year, title, place, publisher, date, document), ...]
     preprocess_type = query_utils.parse_preprocess_word_type("none")
     documents = archives.flatMap(
-        lambda archive: [(document.year, document.title, document.place, document.publisher, document.date, document) for document in list(archive)])
+        lambda archive: [(document.title, document.year, document.place, document.archive.filename, document.num_pages, document.document_type,  document) for document in list(archive)])
     # [(year, title, place, publisher, date, page_string)
     pages = documents.flatMap(
-        lambda year_document: [(year_document[0],year_document[1], year_document[2], year_document[3], year_document[4], 
+        lambda year_document: [(year_document[0],year_document[1], year_document[2], year_document[3], page.code, year_document[4], year_document[5],   
                                     get_page_as_string(page, preprocess_type)) 
-                                       for page in year_document[5]])
+                                       for page in year_document[6]])
 
-    pages.saveAsTextFile("hdfs:///nls.txt")
+    pages.saveAsTextFile("hdfs:///user/at003/rosa/text12.txt")
     return "0"
