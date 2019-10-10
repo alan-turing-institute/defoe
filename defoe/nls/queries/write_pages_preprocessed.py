8b3b69167284f0ef47b5aab30088080a6ded6b46
rosafilgueira <rosa.filgueira@ed.ac.uk>
2019-10-10 18:19:24 +0100

    improving prep query

--- defoe/nls/queries/write_pages_preprocessed.py
+++ defoe/nls/queries/write_pages_preprocessed.py
@@ -12,7 +12,7 @@ def do_query(archives, config_file=None, logger=None):
     Writes raw pages as string to HDFS textfiles, and some metadata associated with each document.
 
     Non preprocessed steps are applied to the words extracted from pages.
-    Metadata collected: Tittle, year, place, archive filename, page filename, type of archive
+    Metadata collected: Tittle, year, place, archive filename, page filename, page id, num pages, type of archive, type of preprocess treatment
  
     config_file must be the path to a configuration file with the preoprocessed type to apply to the pages' words
 
@@ -32,9 +32,9 @@ def do_query(archives, config_file=None, logger=None):
         lambda archive: [(document.title, document.year, document.place, document.archive.filename, document.num_pages, document.document_type,  document) for document in list(archive)])
     # [(year, title, place, publisher, date, page_string)
     pages = documents.flatMap(
-        lambda year_document: [(year_document[0],year_document[1], year_document[2], year_document[3], page.code, year_document[4], year_document[5],   
+        lambda year_document: [(year_document[0],year_document[1], year_document[2], year_document[3], page.code, page.page_id, year_document[4], year_document[5], perprocess_type,   
                                     get_page_as_string(page, preprocess_type)) 
                                        for page in year_document[6]])
 
-    pages.saveAsTextFile("hdfs:///user/at003/rosa/text13.txt")
+    pages.saveAsTextFile("hdfs:///user/at003/rosa/text16.txt")
     return "0"
